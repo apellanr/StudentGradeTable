97ca104ee3104fbc0a26388927824d2f97713d5c
apellanr <ryan.apellanes@gmail.com>
2017-03-16 12:31:10 -0700

    html skeleton

---
+++ script.js
@@ -0,0 +1,64 @@
+// recompute highest and lowest grade with red and green table rows
+
+/**
+ * Define all global variables here
+ */
+/**
+ * student_array - global array to hold student objects
+ * @type {Array}
+ */
+
+/**
+ * inputIds - id's of the elements that are used to add students
+ *      @type {string[]}
+ */
+
+/**
+ * addClicked - Event Handler when user clicks the add button
+ *      only call other functions
+ */
+
+/**
+ * cancelClicked - Event Handler when user clicks the cancel button, should clear out student form
+ *      only call other functions
+ */
+
+/**
+ * addStudent - creates a student objects based on input fields in the form and adds the object to global student array
+ *      written in such a way that you should not get the data from the inputs directly
+ *      @return undefined
+ */
+
+/**
+ * clearAddStudentForm - clears out the form values based on inputIds variable
+ */
+
+/**
+ * calculateAverage - loop through the global student array and calculate average grade and return that value
+ *      @returns {number}
+ *      needs a function that finds min and max
+ */
+
+/**
+ * updateData - centralized function to update the average and call student list update
+ *      no other code but other functions
+ */
+
+/**
+ * updateStudentList - loops through global student array and appends each objects data into the student-list-container > list-body
+ */
+
+/**
+ * addStudentToDom - take in a student object, create html elements from the values and then append the elements
+ *      into the .student_list tbody
+ *      @param studentObj
+ */
+
+/**
+ * reset - resets the application to initial state. Global variables reset, DOM get reset to initial load state
+ */
+
+
+/**
+ * Listen for the document to load and reset the data to the initial state
+ */
