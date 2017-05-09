34e8bd8737b0fdd233705232b2e0ab59489664c1
apellanr <ryan.apellanes@gmail.com>
2017-05-08 18:57:48 -0700

    clear button functioning. was able to create student obj and push to the student arr

--- script.js
+++ script.js
@@ -1,65 +1,99 @@
+// listen for the document to load and reset the data to the initial state
+$(document).ready(createSGT);
 
-/*
- * Define all global variables here
- */
+var studentInfo = null;
 
-/*
- * student_array - global array to hold student objects
- * @type {Array}
- */
+function createSGT() {
+    studentInfo = new StudentGradeTable();
+    studentInfo.init();
+}
 
-/*
- * inputIds - id's of the elements that are used to add students
- * @type {string[]}
-   // selector that selects the elements - need a DOC ready to test selector
- */
+function StudentGradeTable() {
+    var self = this;
+    this.studentArr = []; // will hold student object
+    this.studentName = $("#studentName"); // id's of the elements that are used to add students
+    this.studentCourse = $("#course"); // selector that selects the elements
+    this.studentGrade = $("#studentGrade"); // need a DOC ready to test selector
 
-/**
- * addClicked - Event Handler when user clicks the add button
- */
+    // initialize constructor function
+    this.init = function() {
+        this.addButton = $(".add");
+        this.cancelButton = $(".cancel");
+        this.eventHandlers();
+    };
 
-/**
- * cancelClicked - Event Handler when user clicks the cancel button, should clear out student form
- */
+    // function for event listeners
+    this.eventHandlers = function() {
+        this.addButton.click(this.addClicked);
+        this.cancelButton.click(this.cancelClicked.bind(this)); // was experiencing cancel button error. need to use .bind(this)
+    };
 
-/**
- * addStudent - creates a student objects based on input fields in the form and adds the object to global student array
-   // calls clearAddStudentForm();
- * @return undefined
- */
+    // event handler when user clicks the add button
+    this.addClicked = function() {
+        console.log("add button was clicked");
+        self.addStudent();
+    };
 
-/**
- * clearAddStudentForm - clears out the form values based on inputIds variable
- */
+    // event handler when user clicks the cancel button, should clear out student form too
+    this.cancelClicked = function() {
+        console.log("cancel button clicked");
+        this.clearStudentAddForm();
+    };
 
-/**
- * calculateAverage - loop through the global student array and calculate average grade and return that value
- * @returns {number}
-   // calculate average after all the for loop is complete
-   // only need to calculate at the end of the for loop
- */
+    //creates a student object based on input fields in the form
+    // and adds the object to global student array
+    // calls clearAddStudentForm();
+    // return undefined;
+    this.addStudent = function() {
+        // need to pull values of input fields
+        var studentObject = {
+            name: this.studentName.val(),
+            course: this.studentCourse.val(),
+            grade: this.studentGrade.val()
+        };
+        console.log("student obj test", studentObject);
+        this.studentArr.push(studentObject);
+        this.clearStudentAddForm();
+    };
 
-/**
- * updateData - centralized function to update the average and call student list update
- *
- */
+    // clears out the form values based on inputIds variable
+    this.clearStudentAddForm = function() {
+        self.studentName.val('');
+        self.studentCourse.val('');
+        self.studentGrade.val('');
+    };
 
-/**
- * updateStudentList - loops through global student array and appends each objects data into the student-list-container > list-body
- */
+    // loop through the global student array and calculate average grade
+    // return the value { number }
+    // calculate average at the end of the for loop
+    this.calculateAverage = function() {
 
-/**
- * addStudentToDom - take in a student object, create html elements from the values and then append the elements
- * into the .student_list tbody
- * @param studentObj
- */
+    };
 
-/**
- * reset - resets the application to initial state. Global variables reset, DOM get reset to initial load state
- */
+    // centralized function to update the average and call student list update function
+    this.updateData = function() {
 
+    };
 
-/**
- * Listen for the document to load and reset the data to the initial state
- */
+    // loops through global student arrat and appends each objects data into the table structure
+    // incorrect so modify necessary references : student-list-container > list-body
+    this.updateStudentList = function() {
+
+    };
+
+    // take in a studentObject, create HTML elements from the values
+    // append the element into the .student_list tbody
+    // @ param studentObj
+    this.addStudentToDom = function(studentObj) {
+
+    };
+
+    // resets the application to initial state. global variables reset
+    // DOM get reset to initial load state
+    this.reset = function() {
+        self.studentArr = [];
+
+    };
+
+}
 
