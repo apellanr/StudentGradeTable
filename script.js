2a10889630d68f3ed3275aa57050b904354edfb6
apellanr <ryan.apellanes@gmail.com>
2017-05-09 16:34:01 -0700

    still a little weary on where to place functions. will get back to this later

--- script.js
+++ script.js
@@ -15,34 +15,28 @@ function StudentGradeTable() {
     this.model = null;
     this.view = null;
     this.init = function() {
-      this.model = new Model(this);
-      this.view = new View(this);
-      this.model.init();
-      this.view.init();
+        console.log("init");
+        this.model = new Model(this);
+        this.view = new View(this);
+        // this.model.init();
+        this.view.init();
     };
 
-    this.addButtonClicked = function() {
-        console.log("add button was clicked");
-        this.addStudent();
-        this.updateData();
+    this.reset = function() {
+        this.model.reset();
+        this.view.clearStudentAddForm();
     };
-
-    this.cancelButtonClicked = function() {
-        console.log("cancel button clicked");
-        this.clearStudentAddForm();
-    };
-
-
-
 
     // **** MODEL **** //
     // for the domain layer. adding and removing data
-    function Model(controller) {
-        this.controller = controller;
+    function Model(View) {
+        this.view= View;
         this.studentArr = []; // will hold student object
-
+        this.studentName = $("#studentName"); // id's of the elements that are used to add students
+        this.studentCourse = $("#course"); // selector that selects the elements
+        this.studentGrade = $("#studentGrade"); // need a DOC ready to test selector
         this.init = function() {
-
+            this.addStudent();
         };
 
         // return undefined; ??
@@ -54,13 +48,15 @@ function StudentGradeTable() {
             };
             console.log("student obj test", studentObject);
             this.studentArr.push(studentObject);
-            this.clearStudentAddForm();
+            console.log(this);
+            this.view.view.clearStudentAddForm();
+
         };
 
         this.updateStudentList = function() {
             $("tbody tr").remove(); // removes elements out of the DOM
             for(var i = 0; i < this.studentArr.length; i++) {
-                this.addStudentToDom(this.studentArr[i]);
+                this.view.addStudentToDom(this.studentArr[i]);
             }
         };
 
@@ -80,71 +76,76 @@ function StudentGradeTable() {
         // DOM get reset to initial load state
         this.reset = function() {
             this.studentArr = [];
-            this.clearStudentAddForm();
+            this.view.clearStudentAddForm();
         };
     }
 
-}
+    // **** VIEW **** //
+    function View(controller) {
+        this.controller = controller;
+        // initialize constructor function
+        this.init = function() {
+            this.textInputs = $(".form-control");
+            this.textInputs.keypress(this.validateKeypress);
+            this.addButton = $(".add");
+            this.addButton.click(this.addButtonClicked.bind(this.controller));
+            this.cancelButton = $(".cancel");
+            this.cancelButton.click(this.cancelButtonClicked.bind(this.controller)); // was experiencing cancel button error. need to use .bind(this)
+            // this.eventHandlers();
+        };
 
+        this.validateKeypress = function(event) {
+            if(event.keyCode === 13) {
+                $(".add").click();
+            }
+        };
 
+        this.addButtonClicked = function() {
+            console.log("add button was clicked", this);
+            this.model.addStudent();
+            this.model.updateData();
+        };
 
+        this.cancelButtonClicked = function() {
+            console.log("cancel button clicked");
+            this.clearStudentAddForm();
+        };
 
-// **** VIEW **** //
-function View(controller) {
-    this.controller = controller;
-    this.studentName = $("#studentName"); // id's of the elements that are used to add students
-    this.studentCourse = $("#course"); // selector that selects the elements
-    this.studentGrade = $("#studentGrade"); // need a DOC ready to test selector
-
-    // initialize constructor function
-    this.init = function() {
-        this.textInputs = $(".form-control");
-        this.addButton = $(".add");
-        this.cancelButton = $(".cancel");
-        this.eventHandlers();
-    };
-    // function for event listeners
-    this.eventHandlers = function() {
-        this.textInputs.keypress(this.validateKeypress);
-        this.addButton.click(this.addButtonClicked.bind(this));
-        this.cancelButton.click(this.cancelButtonClicked.bind(this)); // was experiencing cancel button error. need to use .bind(this)
-    };
-
-    this.validateKeypress = function(event) {
-        if(event.keyCode === 13) {
-            $(".add").click();
-        }
-    };
-
-    this.addStudentToDom = function(studentObj) {
-        var $tRow = $("<tr>");
-        var $tdName = $("<td>").text(studentObj.name);
-        var $tdCourse = $("<td>").text(studentObj.course);
-        var $tdGrade = $("<td>").text(studentObj.grade);
-        var $tdDelete = $("<td>");
-        var $deleteButton = $("<button>",{
-            class: "btn btn-danger",
-            text: "Delete",
-            type: "button",
-            id: studentObj.id
-        });
-        console.log('elements created');
-        (function(self){
-            $tdDelete.append($deleteButton);
-            $tRow.append($tdName, $tdCourse, $tdGrade, $tdDelete);
-            $('#tableBody').append($tRow);
-            $tRow.on('click', 'button', function(){
-                var deletePosition = $tRow.index(); // gets index of element relative to selector
-                $tRow.remove(); // need to remove the selected element
-                self.studentArr.splice(deletePosition, 1);
-                self.updateData();
+        this.addStudentToDom = function(studentObj) {
+            console.log("element created");
+            var $tRow = $("<tr>");
+            var $tdName = $("<td>").text(studentObj.name);
+            var $tdCourse = $("<td>").text(studentObj.course);
+            var $tdGrade = $("<td>").text(studentObj.grade);
+            var $tdDelete = $("<td>");
+            var $deleteButton = $("<button>",{
+                class: "btn btn-danger",
+                text: "Delete",
+                type: "button",
+                id: studentObj.id
             });
-        })(this);
-    };
+            console.log('elements created');
+            (function(self){
+                $tdDelete.append($deleteButton);
+                $tRow.append($tdName, $tdCourse, $tdGrade, $tdDelete);
+                $('#tableBody').append($tRow);
+                $tRow.on('click', 'button', function(){
+                    var deletePosition = $tRow.index(); // gets index of element relative to selector
+                    $tRow.remove(); // need to remove the selected element
+                    self.studentArr.splice(deletePosition, 1);
+                    self.updateData();
+                });
+            })(this);
+        };
 
-    this.clearStudentAddForm = function() {
-        this.studentName.val('');
-        this.studentCourse.val('');
-        this.studentGrade.val('');
-    };
+        this.clearStudentAddForm = function() {
+            this.studentName.val('');
+            this.studentCourse.val('');
+            this.studentGrade.val('');
+        };
+    }
 }
+
+
+
+
